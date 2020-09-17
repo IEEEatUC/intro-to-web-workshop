8dee00a44a5f7de0a918de71db47a7a6dc41d851
gargAbhinav4699 <34428698+gargAbhinav4699@users.noreply.github.com>
2020-09-17 15:31:37 -0400

    Finished react demo

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    // Declare a new state variable, which we'll call "count"  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(props.intialCount);
+
+    useEffect(() => {
+        // Use effects to update the title
+        document.title = `You clicked ${count} times`;
+    });
+
+    return (
+        <div className="app-group">
+            <h1 id="app-title">You clicked {count} times</h1>
+            <button className="app-button" onClick={() => setCount(count + 1)}>
+                Increase count
+            </button>
+        </div>
+    );
 }
 
 export default App;
